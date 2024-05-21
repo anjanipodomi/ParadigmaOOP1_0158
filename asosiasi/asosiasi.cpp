a03e00ce294ca2c75f24266c44731cec8ac0f2c8
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-21 12:45:26 +0700

    menambahkan distruktor pasien dan voif tambahdokter dan void cetakdokter

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -7,7 +7,14 @@ class pasien {
 public:
     string nama;
     vector<dokter*> daftar_dokter;
-
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
 };
 
 
